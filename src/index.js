dc2caf851ff110760a8d86d0a1a04e5b2f5d8cd6
xujj <939160741@qq.com>
2018-10-30 09:50:11 +0800

    使用watch

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import _ from 'lodash';
+import j from 'jquery'
 
 function component() {
-  let element = document.createElement('div');
-
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
+  var element = j('<div></div>');
+  element.html(_.join(['Ho', 'webpack'], ' '))
+  return element.get(0);
 }
 
 document.body.appendChild(component());
